d45ff9f964b0260181a27ebce877e3b6235cba23
Alex <alex.chart@turningpointhr.com>
2022-02-10 16:00:31 +0000

    commit tree ace8d4d9c2b96e46d8f805edaaa0772b01727e99

---
+++ js/index.js
@@ -0,0 +1,85 @@
+// http://localhost:3000/monsters/?_limit=50&_page=1
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchMonster()
+    createForm()
+    document.querySelector("#monster-form").addEventListener('submit', (event) => {
+        event.preventDefault()
+        let name = document.querySelector('#monster-name')
+        let age = document.querySelector('#monster-age')
+        let description = document.querySelector('#monster-description')
+
+        monsterObj = {
+            name,
+            age,
+            description
+        }
+
+        console.log(monsterObj)
+
+        postNewMonster(monsterObj)
+})
+
+})
+const createForm = () => {
+    let formContainer = document.querySelector("#create-monster")
+    let form = document.createElement('form')
+    form.id = 'monster-form'
+    let nameInput = document.createElement('input')
+    let nameLabel = document.createElement('label')
+    let ageInput = document.createElement('input')
+    let ageLabel = document.createElement('label')
+    let descriptionInput = document.createElement('input')
+    let descriptionLabel = document.createElement('label')
+    let h2 = document.createElement('h2')
+    let button = document.createElement('button')
+    button.innerText = "MAKE MONSTER MASH!!"
+    nameInput.id = "monster-name"
+    ageInput.id = "monster-age"
+    descriptionInput.id = "monster-description"
+
+    h2.innerHTML = 'Create Monster'
+    nameLabel.innerText = 'name'
+    ageLabel.innerText = 'age'
+    descriptionLabel.innerText = 'description'
+    
+    form.append(nameInput, nameLabel, ageInput, ageLabel, descriptionInput, descriptionLabel, button)
+    formContainer.append(h2, form)
+}
+
+const postNewMonster = (name, age, description) => {
+    fetch('http://localhost:3000/monsters', {
+method: "POST",
+headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+
+body: JSON.stringify({ name, age, description})
+})
+.then (resp => resp.json())
+.then (monster => console.log(monster))
+}
+
+const fetchMonster = () => {
+    let monsterContainer = document.querySelector("#monster-container")
+    console.log(monsterContainer)
+    fetch('http://localhost:3000/monsters/?_limit=50&_page=1')
+    .then (resp => resp.json())
+    .then(monstersData => {
+        console.log(monstersData)
+        monstersData.forEach(monster => {
+            
+            let card = document.createElement('div')
+            let name = document.createElement('h2')
+            let age = document.createElement('h4')
+            let description = document.createElement('p')
+            name.innerText = monster.name
+            age.innerText = `Age: ${monster.age}`
+            description.innerText = `Bio: ${monster.description}`
+
+            card.append(name, age, description) 
+            monsterContainer.append(card)
+        })
+    })
+}
